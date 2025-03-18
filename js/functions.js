99d011ae9bc045bf1d942ada814ec15cf73e281d
VicKnyazeva <v.b.knyazeva@hotmail.com>
2025-03-18 20:48:39 +0700

    Module 5 Task 2

--- js/functions.js
+++ js/functions.js
@@ -56,3 +56,52 @@ extractNumber('а я томат'); // NaN
 extractNumber(2023); // 2023
 extractNumber(-1); // 1
 extractNumber(1.5); // 15
+
+function checkPeriod(startTime, endTime, meetingStart, meetingLength) {
+
+  function convertToMinutes(time) {
+    if (typeof time !== 'string') {
+      return NaN;
+    }
+    const timeArr = time.split(':');
+    if (timeArr.length !== 2) {
+      return NaN;
+    }
+    let v = Number(timeArr[0]);
+    if (isNaN(v) || !Number.isInteger(v)) {
+      return NaN;
+    }
+
+    let timeValue = v * 60;
+
+    v = Number(timeArr[1]);
+    if (isNaN(v) || !Number.isInteger(v)) {
+      return NaN;
+    }
+    timeValue += v;
+    return timeValue;
+  }
+
+  if (!Number.isInteger(meetingLength)) {
+    return false;
+  }
+  const timeStart = convertToMinutes(startTime);
+  if (isNaN(timeStart)) {
+    return false;
+  }
+  const timeEnd = convertToMinutes(endTime);
+  if (isNaN(timeEnd)) {
+    return false;
+  }
+  const timeMeetingStart = convertToMinutes(meetingStart);
+  if (isNaN(timeMeetingStart)) {
+    return false;
+  }
+  return timeMeetingStart >= timeStart && timeMeetingStart + meetingLength <= timeEnd;
+}
+
+checkPeriod('08:00', '17:30', '14:00', 90); // true
+checkPeriod('8:0', '10:0', '8:0', 120); // true
+checkPeriod('08:00', '14:30', '14:00', 90); // false
+checkPeriod('14:00', '17:30', '08:0', 90); // false
+checkPeriod('8:00', '17:30', '08:00', 900); // false
