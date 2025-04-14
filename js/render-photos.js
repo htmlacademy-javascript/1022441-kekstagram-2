c976d92b44b79544282bc90da0426c72519b943c
Victoria Knyazeva <v.b.knyazeva@hotmail.com>
2025-04-15 02:25:49 +0700

    Открывается и закрывается (#7)
    * Создает модальное окно для большой картинки
    
    * fix: Добавляет делегирование событий
    
    * fix

--- js/render-photos.js
+++ js/render-photos.js
@@ -1,3 +1,4 @@
+import {renderFullsizePhoto} from './render-fullsize-photo.js';
 
 const photosContainer = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
@@ -5,16 +6,29 @@ const pictureTemplate = document.querySelector('#picture').content.querySelector
 const renderPhotosList = (pictures) => {
   const pictureFragment = document.createDocumentFragment();
 
-  pictures.forEach(({url, description, likes, comments}) => {
+  pictures.forEach((picture) => {
     const pictureElement = pictureTemplate.cloneNode(true);
-    pictureElement.querySelector('.picture__img').src = url;
-    pictureElement.querySelector('.picture__img').alt = description;
-    pictureElement.querySelector('.picture__likes').textContent = likes;
-    pictureElement.querySelector('.picture__comments').textContent = comments.length;
+
+    pictureElement.querySelector('.picture__img').src = picture.url;
+    pictureElement.querySelector('.picture__img').alt = picture.description;
+    pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+    pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
+    pictureElement.setAttribute('data-picture-id', picture.id);
     pictureFragment.appendChild(pictureElement);
   });
 
   photosContainer.appendChild(pictureFragment);
+
+  photosContainer.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    const pictureElement = evt.target.closest('[data-picture-id]');
+    if (!pictureElement) {
+      return;
+    }
+    const pictureId = +(pictureElement.getAttribute('data-picture-id'));
+    const clickedPicture = pictures.find((picture) => picture.id === pictureId);
+    renderFullsizePhoto(clickedPicture);
+  });
 };
 
 export {renderPhotosList};
