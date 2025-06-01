a93b2fa513701f76e74c2e2573d4ba91b9ece803
Victoria Knyazeva <v.b.knyazeva@hotmail.com>
2025-06-01 21:45:43 +0700

    Б12. В итоговом коде проекта находятся только те файлы, которые были на момент создания репозитория, которые были получены в патчах, и файлы, созданные по заданию.

--- js/util.js
+++ js/util.js
@@ -1,32 +1,6 @@
 
 const ERROR_DISPLAY_TIME = 5000;
 
-const getRandomInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-
-  return Math.floor(result);
-};
-
-const getUniqInteger = (min, max) => {
-  const previousValues = [];
-
-  return () => {
-    let currentValue = getRandomInteger(min, max);
-    if (previousValues.length >= (max - min + 1)) {
-      return null;
-    }
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomInteger(min, max);
-    }
-    previousValues.push(currentValue);
-    return currentValue;
-  };
-};
-
-const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
 const getDocumentKeydownHandler = (callback) => (evt) => {
@@ -121,4 +95,4 @@ const debounce = (callback, timeoutDelay = 500) => {
   };
 };
 
-export {getRandomInteger, getUniqInteger, getRandomArrayElement, getDocumentKeydownHandler, preventEscPropagation, showSuccess, showStartError, showError, isEscapeKey, debounce};
+export {getDocumentKeydownHandler, preventEscPropagation, showSuccess, showStartError, showError, isEscapeKey, debounce};
